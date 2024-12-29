55ae6a48584dd31b8e160afbcb2a21e2df1c8cab
Jason Whittaker <jason.whittaker@7peakssoftware.com>
2024-12-29 15:29:03 +0700

    chore: set up initial get tasks

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ import (
 var tmpl *template.Template
 var db *sql.DB
 
+type Task struct {
+	Id   int
+	Task string
+	Done bool
+}
+
 func init() {
 	tmpl, _ = template.ParseGlob("templates/*.html")
 
@@ -20,8 +26,7 @@ func init() {
 
 func initDB() {
 	var err error
-	// Initialize the db variable
-	db, err = sql.Open("mysql", "root:root@(127.0.0.1:5426)/testdb?parseTime=true")
+	db, err = sql.Open("pgx", "root:root@(127.0.0.1:5432)/crud?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,6 +45,66 @@ func main() {
 	initDB()
 	defer db.Close()
 
+	gRouter.HandleFunc("/", Homepage)
+
+	//Get Tasks
+	gRouter.HandleFunc("/tasks", fetchTasks).Methods("GET")
+
+	//Fetch Add Task Form
+	gRouter.HandleFunc("/newtaskform", getTaskForm)
+
 	http.ListenAndServe(":4000", gRouter)
+
+}
+
+func Homepage(w http.ResponseWriter, r *http.Request) {
+
+	tmpl.ExecuteTemplate(w, "home.html", nil)
+
+}
+
+func fetchTasks(w http.ResponseWriter, r *http.Request) {
+	todos, _ := getTasks(db)
+	//fmt.Println(todos)
+
+	//If you used "define" to define the template, use the name you gave it here, not the filename
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func getTaskForm(w http.ResponseWriter, r *http.Request) {
+
+	tmpl.ExecuteTemplate(w, "addTaskForm", nil)
+}
+
+func getTasks(dbPointer *sql.DB) ([]Task, error) {
+
+	query := "SELECT id, task, done FROM tasks"
+
+	rows, err := dbPointer.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var tasks []Task
+
+	for rows.Next() {
+		var todo Task
+
+		rowErr := rows.Scan(&todo.Id, &todo.Task, &todo.Done)
+
+		if rowErr != nil {
+			return nil, err
+		}
+
+		tasks = append(tasks, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
 
 }
