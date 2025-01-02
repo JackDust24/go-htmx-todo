e717f98deea2e3ec094aca69dc2ecad8207861d7
Jason Whittaker <jason.whittaker@7peakssoftware.com>
2025-01-02 15:19:41 +0700

    chore: applied postgres changes to go file

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/JackDust24/go-htmx-crud
 go 1.23.4
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"text/template"
 
 	"github.com/gorilla/mux"
@@ -25,8 +28,9 @@ func init() {
 }
 
 func initDB() {
+
 	var err error
-	db, err = sql.Open("pgx", "root:root@(127.0.0.1:5432)/crud?parseTime=true")
+	db, err = sql.Open("pgx", "postgres://postgres:root@127.0.0.1:5432/crud?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,16 +39,16 @@ func initDB() {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 func main() {
 
 	gRouter := mux.NewRouter()
 
-	//Setup MySQL
 	initDB()
-	defer db.Close()
 
+	defer db.Close()
 	gRouter.HandleFunc("/", Homepage)
 
 	//Get Tasks
@@ -52,6 +56,18 @@ func main() {
 
 	//Fetch Add Task Form
 	gRouter.HandleFunc("/newtaskform", getTaskForm)
+
+	//Add Task
+	gRouter.HandleFunc("/tasks", addTask).Methods("POST")
+
+	//Fetch Update Form
+	gRouter.HandleFunc("/gettaskupdateform/{id}", getTaskUpdateForm).Methods("GET")
+
+	//Update Task
+	gRouter.HandleFunc("/tasks/{id}", updateTask).Methods("PUT", "POST")
+
+	//Delete Task
+	gRouter.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 
 	http.ListenAndServe(":4000", gRouter)
 
@@ -63,9 +79,43 @@ func Homepage(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func fetchTasks(w http.ResponseWriter, r *http.Request) {
+// ADDS
+func addTask(w http.ResponseWriter, r *http.Request) {
+
+	task := r.FormValue("task")
+
+	fmt.Println(task)
+
+	done := false
+
+	query := `INSERT INTO tasks."Tasks" (task, done) VALUES ($1, $2);`
+
+	stmt, err := db.Prepare(query)
+
+	if err != nil {
+		log.Fatal("Error adding task", err)
+	}
+	defer stmt.Close()
+
+	_, executeErr := stmt.Exec(task, done)
+
+	if executeErr != nil {
+		log.Fatal("Execution Error adding task", executeErr)
+	}
+
+	// Return a new list of Todos
 	todos, _ := getTasks(db)
-	//fmt.Println(todos)
+
+	//You can also just send back the single task and append it
+	//I like returning the whole list just to get everything fresh, but this might not be the best strategy
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+
+}
+
+func fetchTasks(w http.ResponseWriter, r *http.Request) {
+
+	todos, _ := getTasks(db)
+	fmt.Println(todos)
 
 	//If you used "define" to define the template, use the name you gave it here, not the filename
 	tmpl.ExecuteTemplate(w, "todoList", todos)
@@ -76,9 +126,25 @@ func getTaskForm(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "addTaskForm", nil)
 }
 
+func getTaskUpdateForm(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	taskId, _ := strconv.Atoi(vars["id"])
+
+	task, err := getTaskByID(db, taskId)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	tmpl.ExecuteTemplate(w, "updateTaskForm", task)
+
+}
+
 func getTasks(dbPointer *sql.DB) ([]Task, error) {
 
-	query := "SELECT id, task, done FROM tasks"
+	query := `SELECT id, task, done FROM tasks."Tasks"`
 
 	rows, err := dbPointer.Query(query)
 
@@ -106,5 +172,136 @@ func getTasks(dbPointer *sql.DB) ([]Task, error) {
 	}
 
 	return tasks, nil
+
+}
+
+func getTaskByID(dbPointer *sql.DB, id int) (*Task, error) {
+
+	query := `SELECT id, task, done FROM tasks."Tasks" WHERE id = $1`
+
+	var task Task
+
+	row := dbPointer.QueryRow(query, id)
+	err := row.Scan(&task.Id, &task.Task, &task.Done)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("No task was found with task %d", id)
+		}
+		return nil, err
+	}
+
+	return &task, nil
+
+}
+
+// UPDATES
+func updateTask(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	taskItem := r.FormValue("task")
+
+	var taskStatus bool
+
+	fmt.Println(r.FormValue("done"))
+
+	switch strings.ToLower(r.FormValue("done")) {
+	case "yes", "on":
+		taskStatus = true
+	case "no", "off":
+		taskStatus = false
+	default:
+		taskStatus = false
+	}
+
+	taskId, _ := strconv.Atoi(vars["id"])
+
+	task := Task{
+		taskId, taskItem, taskStatus,
+	}
+
+	updateErr := updateTaskById(db, task)
+
+	if updateErr != nil {
+		log.Fatal(updateErr)
+	}
+
+	todos, _ := getTasks(db)
+
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func updateTaskById(dbPointer *sql.DB, task Task) error {
+
+	query := `UPDATE tasks."Tasks" SET task = $1, done = $2 WHERE id = $3`
+
+	result, err := dbPointer.Exec(query, task.Task, task.Done, task.Id)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		fmt.Println("No rows updated")
+	} else {
+		fmt.Printf("%d row(s) updated\n", rowsAffected)
+	}
+
+	return nil
+
+}
+
+// DELETES
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	taskId, _ := strconv.Atoi(vars["id"])
+
+	err := deleteTaskWithID(db, taskId)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	todos, _ := getTasks(db)
+
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func deleteTaskWithID(dbPointer *sql.DB, id int) error {
+
+	query := `DELETE FROM tasks."Tasks" WHERE id = $1`
+
+	stmt, err := dbPointer.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("no task found with id %d", id)
+	}
+
+	fmt.Printf("Deleted %d task(s)\n", rowsAffected)
+	return nil
 
 }
